88ef89670885654741a6b322cbf58abac61191bc
TusharGharge <tushargharge1111@gmail.com>
2024-04-13 20:07:56 +0530

    data base env

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ import psycopg2
 
 # SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://yourbankuser:ZoDUVYGZjR2mroCALa0Qf4kAxtpp6TNI@dpg-cob7gc21hbls73anrrqg-a.oregon-postgres.render.com:5432/yourbank_lt8j'
+SQLALCHEMY_DATABASE_URL = f'{settings.DATABASE_URL}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
